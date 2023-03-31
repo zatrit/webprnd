5414b4af568023942eb8f7b94734dae74fb10720
zatrit <52380960+zatrit@users.noreply.github.com>
2023-03-31 11:27:52 +0300

    Улучшено пользовательское взаимодействие

--- web/script/editor.ts
+++ web/script/editor.ts
@@ -3,14 +3,19 @@ import * as vis from "vis-network";
 let nodeCounter: number = 0;
 let edgeCounter: number = 0;
 let network: vis.Network;
+let hoverState: HoverState;
+let mouseState: MouseState;
 
 const nodesArray = [{ id: 0, label: "Node 0" }];
 const nodes = new vis.DataSet<vis.Node>(nodesArray);
 const edges = new vis.DataSet<vis.Edge>([]);
 
 document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("editor-container")! as HTMLCanvasElement;
     const style = getComputedStyle(document.body);
-    const container = document.getElementById("editor-container")!;
+
+    hoverState = hoverListener(container);
+    mouseState = mouseListener(container);
 
     const data = { nodes, edges, };
     const options: vis.Options = {
@@ -33,6 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         interaction: {
             multiselect: true,
+            navigationButtons: false,
+            selectConnectedEdges: false,
+            keyboard: true
         },
     };
 
@@ -45,11 +53,16 @@ document.addEventListener("DOMContentLoaded", () => {
         else if (e.code == "Delete") {
             deleteSelected();
         }
-        else if (e.code == "KeyA" && e.ctrlKey) {
-            selectAll();
-        }
         else if (e.code == "KeyA") {
-            connectSelected();
+            if (e.ctrlKey) {
+                e.preventDefault();
+                selectAll();
+            } else {
+                connectSelected();
+            }
+        }
+        else if (e.code == "Escape") {
+            network.unselectAll();
         }
     };
 });
@@ -63,7 +76,8 @@ function pairwise<T>(arr: T[], func: (cur: T, next: T) => void) {
 
 function addNode() {
     nodeCounter++;
-    nodes.add({ id: nodeCounter, label: "Node " + nodeCounter });
+    let pos = hoverState.hovered ? network.DOMtoCanvas(mouseState) : network.getViewPosition();
+    nodes.add({ id: nodeCounter, label: "Node " + nodeCounter, x: pos.x, y: pos.y });
 }
 
 function deleteSelected() {
@@ -90,4 +104,25 @@ function connectSelected() {
     });
 
     network.unselectAll();
+}
+
+type HoverState = { hovered: boolean };
+type MouseState = { x: number, y: number };
+
+function hoverListener(element: HTMLElement): HoverState {
+    const state = { hovered: false };
+
+    element.addEventListener("mouseover", _ => state.hovered = true);
+    element.addEventListener("mouseout", _ => state.hovered = false);
+    return state;
+}
+
+function mouseListener(element: HTMLElement): MouseState {
+    const state = { x: 0, y: 0 };
+    element.addEventListener("mousemove", (e: MouseEvent) => {
+        state.x = e.x;
+        state.y = e.y;
+    });
+
+    return state;
 }
