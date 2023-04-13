8f464012bfa577010e1d2124b2dd284f4b567fd6
Raymante <76446768+Raymante@users.noreply.github.com>
2023-04-13 17:18:51 +0300

    Update test_crypto.py

--- test/test_crypto.py
+++ test/test_crypto.py
@@ -7,23 +7,33 @@ expires = int(time() + 9999999)
 sign_key = SigningKey.generate()
 verf_key: VerifyingKey = sign_key.verifying_key  # type: ignore
 
+invalid_cases = [
+    "FIRST:SECOND:THIRD:AAA",
+    "FIRST:SECOND:THIRD:AAA:BBB",
+    "FcCx_VPCl_Xd0IMZNt0fgI9pOP5U3gT1D1Jy1Ce5kfvGai9Gz1f86UZ-0eKJj6yq:AAAAAGQ26FU=:YXBp:emF0cml0",
+    "NOT TOKEN AT ALL"
+]
 
-def validate_token(token: str, verify_key: VerifyingKey):
+
+def validate_token(token: str, role: str, verify_key: VerifyingKey):
     return verify_token(token, role, int(time()), lambda _: verify_key)
 
 
 def test_validation():
     token = token_for(login, sign_key, expires, role)
-    assert validate_token(token, verf_key)
+    assert validate_token(token, role, verf_key)
 
 
 def test_invalidation():
-    cases = [
-        "FIRST:SECOND:THIRD:AAA",
-        "FIRST:SECOND:THIRD:AAA:BBB",
-        "FcCx_VPCl_Xd0IMZNt0fgI9pOP5U3gT1D1Jy1Ce5kfvGai9Gz1f86UZ-0eKJj6yq:AAAAAGQ26FU=:YXBp:emF0cml0",
-        "NOT TOKEN AT ALL"
-    ]
+    for token in invalid_cases:
+        assert not validate_token(token, role, verf_key)
 
-    for token in cases:
-        assert not validate_token(token, verf_key)
+
+def test_role_invalidation():
+    token = token_for(login, sign_key, expires, role)
+    assert not validate_token(token, "invalid role", verf_key)
+
+
+def test_role_and_token_invalidation():
+    for token in invalid_cases:
+        assert not validate_token(token, "invalid role", verf_key)
