e45444e7ad9239525557b7b69b72d224fbd5b9c7
zatrit <52380960+zatrit@users.noreply.github.com>
2023-04-15 09:25:18 +0300

    Вынесен index в отдельную ветку

--- pages/other.py
+++ pages/other.py
@@ -2,12 +2,6 @@ from flask import render_template
 from . import blueprint, requires_auth
 
 
-@blueprint.route("/")
-@blueprint.route("/index")
-def index():
-    return render_template("index.html", title="WebPRND")
-
-
 @blueprint.route("/editor")
 @requires_auth
 def editor():
